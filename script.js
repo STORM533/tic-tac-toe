aee2521b4566004cecac3101e1cc3024fb4b3b63
STORM533 <146557098+STORM533@users.noreply.github.com>
2025-08-18 22:12:21 +0530

    player creation and gameboard created

---
+++ script.js
@@ -0,0 +1,22 @@
+//this is a factory function creating player.
+function createPlayer (player , marker){
+    return{player , marker};
+}
+//this is creating array using IIFEs
+const gameBoard = (function (){
+    let arr =  Array["","","","","","","","",""];
+    return{arr};
+})();
+//conditions for winning
+function winning (){
+    const winPattern = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    
+}   
+//creates player and starts game
+function gameControl(){
+    const player1 = createPlayer("player1" , "X" );
+    const player2 = createPlayer("player2" , "O");
+
+}
+
+
