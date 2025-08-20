b6cbfe497a90671c110914349590586d3826008e
STORM533 <146557098+STORM533@users.noreply.github.com>
2025-08-20 13:07:29 +0530

    added start and reset button modified the UI experience game stop functionality added separated the player creation into an IIFE

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 //this is a factory function creating player.
-
 function createPlayer (player , marker){
     return{player , marker};
 }
+//creating player 1 and 2 using IIFEs
+const player = (function(){
+    const player1 = createPlayer("player 1" , "X");
+    const player2 = createPlayer("player 2" , "O");
+    return{player1 , player2};
+})();
 //this is creating array using IIFEs
 const gameBoard = (function (){
     let arr = ["","","","","","","","",""];
     return{arr};
 })();
-//conditions for winning
+//conditions for winning using IIFEs
 const winning = (function (){
     const winPattern = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     return{winPattern};
@@ -18,19 +23,20 @@ function checkWinner(){
     for(let i = 0;i<winning.winPattern.length; i++){
         let [a , b , c] =winning.winPattern[i];
         if(gameBoard.arr[a] =="X" && gameBoard.arr[b] =="X" && gameBoard.arr[c] == "X"){
-            console.log("player 1 wins");
+            console.log(player.player1.player + " wins");
+            return true;
         }
         if(gameBoard.arr[a] =="O" && gameBoard.arr[b] =="O" && gameBoard.arr[c] == "O"){
-            console.log("player 2 wins");
+            console.log(player.player2.player + " wins");
+            return true;
         }
     }
 }
-  
-//creates player and starts game
+//game working
 function gameControl(){
     let isFirstClick = true;
-    const player1 = createPlayer("player1" , "X" );
-    const player2 = createPlayer("player2" , "O");
+    const player1 = player.player1;
+    const player2 = player.player2;
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((div)=>{
         div.addEventListener("click",()=>{
@@ -38,33 +44,36 @@ function gameControl(){
             if(isFirstClick){
                 if(gameBoard.arr[index] == ""){
                     div.innerHTML = player1.marker;
+                    div.style.color = "#ff7c01d3";
                     gameBoard.arr[index]=player1.marker;
-                    checkWinner();
+                    if(checkWinner()){
+                        gameOff();
+                    }
                 }
             }else{
                 if(gameBoard.arr[index] == ""){
                     div.innerHTML = player2.marker;
+                    div.style.color = "#4907e2c4"
                     gameBoard.arr[index]=player2.marker;
-                    checkWinner();
+                    if(checkWinner()){
+                        gameOff();  
+                    }
                 }
             }
             isFirstClick = !isFirstClick;
         });
     });
 }
+//stops game after a winner
+function gameOff(){
+    document.querySelectorAll(".box").forEach(box =>{
+        box.style.pointerEvents = "none";
+    });
+}
 //display 
 function displayController(){
-    const container = document.querySelector("body");
-    const display = document.createElement("div");
-    display.classList.add("disp");
-    container.appendChild(display);
-    const disp = document.querySelector(".disp");
-    const player1 = document.createElement("div");
-    const player2 = document.createElement("div");
-    player1.classList.add("player1");
-    player2.classList.add("player2");
-    disp.appendChild(player1);
-    disp.appendChild(player2);
+
+    
 }
 
 console.log(gameBoard.arr);
